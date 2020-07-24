5e80a5cb12d8bca55636e6d53722a4da47edd2c8
kgonzalez1219 <kat@MSI.localdomain>
2020-07-23 22:21:36 -0700

    main content

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
 'use strict';
 
+
+var answerOne = prompt('What a day, no?');
+console.log(answerOne);
+
+if (answerOne === true) {
+  console.log('Make sure to take time for yourself');
+} else {
+  console.log('Glad to hear you\'re well');
+}
+
+var myBool = true;
+var anotherBool = false;
+var thirdBool = true;
+
+if (myBool && thirdBool) {
+  console.log('myBool and thirdBool are true');
+}
+else if (myBool || anotherBool) {
+  console.log('myBool OR anotherBool is true');
+}
+else if (thirdBool) {
+  console.log('thirdBool is true');
+}
