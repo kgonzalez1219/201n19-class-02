0a6638b8a3609475fd174df0a0b9d165e6589664
kgonzalez1219 <kat@MSI.localdomain>
2020-07-23 23:28:20 -0700

    add alert boxes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 
 
 var userName = prompt('Hey there, What\'s your name?');
-console.log(userName);
+//console.log(userName);
+
+
 {
   document.getElementById("welcome").innerHTML = 
   "Hey" + userName + "How are you?";
@@ -11,19 +13,54 @@ console.log(userName);
 
 
 var answerTwo = prompt('Do I like hiking?');
-console.log(answerTwo);
+//console.log(answerTwo);
+
+if(answerTwo == true) {
+alert('Yes! I love Hiking');
+}
+else if(answerTwo == false) {
+  alert("I actually love Hiking");
+}
 
 var answerThree = prompt('Is reading one of my favorite hobbies?');
-console.log(answerThree);
+//console.log(answerThree);
+
+if(answerThree == true) {
+  alert('I would live in a book if I could!');
+}
+else if(answerThree == false) {
+  alert('I do love to read.');
+}
 
 var answerFour = prompt('Do I love animals?');
-console.log(answerFour);
+//console.log(answerFour);
+
+if(answerFour == true) {
+  alert('I do, I love them!');
+}
+else if(answerFour == false) {
+  alert('I do like animals.');
+}
 
 var answerFive = prompt('Is my favorite genre of music Polka?');
-console.log(answerFive);
+//console.log(answerFive);
+
+if(answerFive == true) {
+  alert('Nope, I do love Wierd Al though.');
+}
+else if(answerFive == false) {
+  alert('Right, I couldn\'t pick just one though.');
+}
 
 var answerSix = prompt('Have I wasted untold hours of my life on video games?');
-console.log(answerSix);
+//console.log(answerSix);
+
+if(answerSix == true) {
+  alert('YES! So many hours ');
+}
+else if(answerSix == false) {
+  alert('Yes, so many hours!');
+}
 
 
 
