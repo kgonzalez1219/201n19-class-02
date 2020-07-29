7e423fbdd63ed857df33128971e10ea886b53bcc
kgonzalez1219 <kat@MSI.localdomain>
2020-07-29 16:05:16 -0700

    tweak questions

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
+//greeting
 
-var userName = prompt('Hey there, What\'s your name?');
+var userName = prompt('Hey there, What\'s your name?').toUpperCase();
 alert("Hey " + userName + " hope you\'re well today");
 //console.log(userName);
 
@@ -12,53 +13,56 @@ alert("Hey " + userName + " hope you\'re well today");
 }
 
 
+//5 questions
 
 var answerTwo = prompt('Do I like hiking?');
 //console.log(answerTwo);
 
-if (answerTwo === "Yes" || "Y") {
+if (answerTwo === "Yes" || answerTwo === "Y") {
   alert('Yes! I love Hiking');
-}else if (answerTwo === "No" || "N") {
+}else if (answerTwo === "No" || answerTwo === "N") {
   alert("I actually love Hiking");
 }
 
 var answerThree = prompt('Is reading one of my favorite hobbies?');
 //console.log(answerThree);
 
-if (answerThree === "Yes" || "Y") {
+if (answerThree === "Yes" || answerThree === "Y") {
   alert('I would live in a book if I could!');
-}else if (answerThree === "No" || "N") {
+}else if (answerThree === "No" || answerThree === "N") {
   alert('I do love to read.');
 }
 
 var answerFour = prompt('Do I love animals?');
 //console.log(answerFour);
 
-if (answerFour === "Yes" || "Y") {
+if (answerFour === "Yes" || answerFour === "Y") {
   alert('I do, I love them!');
-}else if (answerFour === "No" || "N") {
+}else if (answerFour === "No" || answerFour === "N") {
   alert('I do like animals.');
 }
 
 var answerFive = prompt('Is my favorite genre of music Polka?');
 //console.log(answerFive);
 
-if (answerFive === "Yes" || "Y") {
+if (answerFive === "Yes" || answerFive === "Y") {
   alert('Nope, I do love Wierd Al though.');
-}else if (answerFive === "No" || "N") {
+}else if (answerFive === "No" || answerFive === "N") {
   alert('Right, I couldn\'t pick just one though.');
 }
 
 var answerSix = prompt('Have I wasted untold hours of my life on video games?');
 //console.log(answerSix);
 
-if (answerSix === "Yes" || "Y") {
+if (answerSix === "Yes" || answerSix === "Y") {
   alert('YES! So many hours ');
-}else if (answerSix === "No" || "N") {
+}else if (answerSix === "No" || answerSix === "N") {
   alert('Yes, so many hours!');
 }
 
-/*var userGuess;
+//two new questions - not working
+
+var userGuess;
 var userGuessRight
 
 while (userGuessRight) {
@@ -76,9 +80,6 @@ while (userGuessRight) {
 alert('You got it!')
 
 
-var stuff = stuff Array(yes, yes, yes,yes);
-
-for (var stuff = yes; stuff);
 
 
 
@@ -86,6 +87,7 @@ for (var stuff = yes; stuff);
 
 
 
+//
  /* if (answerOne === true) {
     console.log('Make sure to take time for yourself');
   } else {
