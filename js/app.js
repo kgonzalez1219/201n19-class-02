bb5799f28fa6400a873a9e3ad737f97e518284ba
kgonzalez1219 <kat@MSI.localdomain>
2020-08-07 15:02:32 -0700

    add better guessing game

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 var userName;
-var score;
+
 
 //greeting
 
@@ -9,7 +9,7 @@ function greeting() {
   alert("Hey " + userName + " hope you\'re well today!");
   //console.log(userName);
 
-  
+
   document.getElementById("welcome").innerHTML =
 
     "Hey " + userName + " hope you\'re well today";
@@ -20,33 +20,32 @@ greeting();
 //5 questions
 
 function questionOne() {
-var answerTwo = prompt('Do I like hiking?').toLowerCase();
-//console.log(answerTwo);
-if (answerTwo === "Yes" || answerTwo === "Y") {
-  alert('Yes! I love Hiking');
-  score++;
-}else if (answerTwo === "No" || answerTwo === "N") {
-  alert("I actually love Hiking");
-}
+  var answerTwo = prompt('Do I like hiking?');
+  if (answerTwo === "Yes" || answerTwo === "Y")
+    alert('Yes! I love Hiking');
+  else if (answerTwo === "No" ||
+    answerTwo === "N") {
+    alert("I actually love Hiking");
+  }
 }
 
 questionOne();
 
 
-function questionTwo(){
-var answerThree = prompt('Is reading one of my favorite hobbies?').toLowerCase();
-//console.log(answerThree);
+function questionTwo() {
+  var answerThree = prompt('Is reading one of my favorite hobbies?');
 
-if (answerThree === "Yes" || answerThree === "Y") {
-  alert('I would live in a book if I could!');
-  score++;
-}else if (answerThree === "No" || answerThree === "N") {
-  alert('I do love to read.');
 
-  "Hey " + userName + " hope you\'re well today";
-  
+  if (answerThree === "Yes" || answerThree === "Y") {
+    alert('I would live in a book if I could!');
 
-}
+  } else if (answerThree === "No" || answerThree === "N") {
+    alert('I do love to read.');
+
+    "Hey " + userName + " hope you\'re well today";
+
+
+  }
 }
 
 
@@ -55,15 +54,15 @@ questionTwo();
 
 
 function questionThree() {
-var answerFour = prompt('Do I love animals?').toLowerCase();
-//console.log(answerFour);
+  var answerFour = prompt('Do I love animals?');
 
-if (answerFour === "Yes" || answerFour === "Y") {
-  alert('I do, I love them!');
-  score++;
-}else if (answerFour === "No" || answerFour === "N") {
-  alert('I do like animals.');
-}
+
+  if (answerFour === "Yes" || answerFour === "Y") {
+    alert('I do, I love them!');
+
+  } else if (answerFour === "No" || answerFour === "N") {
+    alert('I do like animals.');
+  }
 }
 
 
@@ -72,37 +71,46 @@ questionThree();
 
 
 function questionFour() {
-var answerFive = prompt('Is my favorite genre of music Polka?').toLowerCase();
-//console.log(answerFive);
+  var answerFive = prompt('Is my favorite genre of music Polka?');
 
-if (answerFive === "Yes" || answerFive === "Y") {
-  alert('Nope, I do love Wierd Al though.');
-  score++;
-}else if (answerFive === "No" || answerFive === "N") {
-  alert('Right, I couldn\'t pick just one though.');
+
+  if (answerFive === "Yes" || answerFive === "Y") {
+    alert('Nope, I do love Wierd Al though.');
+
+  } else if (answerFive === "No" || answerFive === "N") {
+    alert('Right, I couldn\'t pick just one genre though.');
+  }
 }
-}
-
-
 
 questionFour();
 
 
-
-
 function questionFive() {
-var answerSix = prompt('Have I wasted untold hours of my life on video games?').toLowerCase();
-//console.log(answerSix);
+  var answerSix = prompt('Have I wasted untold hours of my life on video games?');
 
-if (answerSix === "Yes" || answerSix === "Y") {
-  alert('YES! So many hours ');
-  score++;
-}else if (answerSix === "No" || answerSix === "N") {
-  alert('Yes, so many hours!');
+
+  if (answerSix === "Yes" || answerSix === "Y") {
+    alert('YES! So many hours ');
+
+  } else if (answerSix === "No" || answerSix === "N") {
+    alert('Yes, so many hours!');
+  }
+
+  questionFive();
+
+
 }
 
-questionFive();
 
 
+function guessingGame() {
+  num = prompt("Guess a number between 1 and 10!");
+  if (num < 5 )
+  alert("Too low");
+  else if (num > 5)
+  alert("Too High!");
+  else alert("You got it!");
 }
+
+guessingGame();
 
